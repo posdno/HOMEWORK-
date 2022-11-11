9fa36622e3cc2b5baa38ebc3e27c12d6e8371ef9
posdno <molodkina.dasha@mail.ru>
2022-11-12 01:02:48 +0300

    Добавили код3

---
+++ HW003/Program.cs
@@ -0,0 +1,11 @@
+﻿int a = new Random().Next(1,  10);
+if (a % 2 ==0)
+{
+    Console.Write(a);
+    Console.WriteLine("- четное число");
+}
+else
+{
+    Console.Write(a);
+    Console.WriteLine("- нечетное число");
+}
