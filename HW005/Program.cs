adbca7f67c9ce4a5ff4cc7578de40db2aa41c1af
posdno <molodkina.dasha@mail.ru>
2022-11-13 00:14:00 +0300

    Добавили код5

---
+++ HW005/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write ("Введите трехзначное число:   ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n<0) n = -n;
+    if(n<100 || n>1000) Console.Write("Вы ввели нетрехзначное число, повторите ввод");
+    else 
+    {
+      Console.Write("Вторая цифра этого числа: ");
+      Console.WriteLine(n/10%10);
+    }
