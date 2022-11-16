3b2489f58f78121daa61b2f6727f9911458397d9
posdno <molodkina.dasha@mail.ru>
2022-11-16 21:30:18 +0300

    Добавили код6

---
+++ HW006/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число  ");
+int n=Convert.ToInt32(Console.ReadLine());
+if (n < 0) n = -n;
+while (n>1000) n = n/100;
+if (n<100) Console.Write("Третьей цифры нет ");
+    else
+    {
+        Console.Write("Третья цифра введенного числа -   ");
+        Console.WriteLine(n%10);
+    }
