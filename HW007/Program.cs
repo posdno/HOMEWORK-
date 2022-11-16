82bae5e359adb9c16c812b7b55b3f005b224eea3
posdno <molodkina.dasha@mail.ru>
2022-11-16 22:05:19 +0300

    Добавили код7

---
+++ HW007/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите цифру, обозначающую день недели  ");
+int d = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine(d);
+
+int[] weeek = {1, 2, 3, 4, 5, 6, 7};
+int n = weeek.Length;
+int index = 0;
+//if (d<1||d>7) Console.WriteLine("повторите ввод");
+
+while(index<n)
+{
+    if (d==weeek[5] || d==weeek[6]) Console.WriteLine(" это выходной");
+    else Console.WriteLine("не выходной день");
+    break;
+}
+index++;
