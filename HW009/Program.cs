956c1a5cd6204975be147ca002804f954df3dfcd
posdno <molodkina.dasha@mail.ru>
2022-11-19 20:34:09 +0300

    Добавилт код9

---
+++ HW009/Program.cs
@@ -0,0 +1,25 @@
+﻿int[] GetHumbers(int count)
+{
+    int[] array = new int[count];
+    
+    for (int i = 0; i < count; i++)
+    {
+        array[i] = (i + 1)*(i + 1)*(i + 1);
+    }
+    return array;
+
+}
+
+string ArrayToString(int[] array)
+{
+    string result = string.Empty;
+    {
+        for (int i = 0; i < array.Length; i++)
+             result = result + array[i] + " ";
+    }
+    return result;
+}
+
+Console.WriteLine(ArrayToString(GetHumbers(3)));
+Console.WriteLine(ArrayToString(GetHumbers(5)));
+Console.WriteLine(ArrayToString(GetHumbers(8)));
