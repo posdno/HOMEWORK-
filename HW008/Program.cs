637b9b84ffdc55251be5a3fe809d87b5a4e84c39
posdno <molodkina.dasha@mail.ru>
2022-11-19 20:17:10 +0300

    Добавили код8

---
+++ HW008/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите пятизначное число  ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n > 100000 || n < 10000) Console.Write("Повторите ввод");
+else
+{
+    if (n / 10000 == n % 10 || n / 1000 % 10 == n / 100 % 10) Console.Write("Введенное число - палиндром");
+    else Console.Write("Введенное число не является палиндромом");
+}
