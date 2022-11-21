bcef3c47d23b532d6b8a00a6c01eb46a9687fd91
posdno <molodkina.dasha@mail.ru>
2022-11-21 21:30:34 +0300

    Добавили код 12

---
+++ HW012/Program.cs
@@ -0,0 +1,19 @@
+﻿int GetNumberSum(int value)
+{
+    int sum = 0;
+    if (value < 0) value = -value;
+    if(value ==0 || value < 10) return sum = value;
+
+    while (value >= 10)
+    {
+        sum = sum + value % 10;
+        value = value/10;
+    }
+    sum = sum + value;
+    return sum;
+
+}
+
+Console.WriteLine(GetNumberSum(452));
+Console.WriteLine(GetNumberSum(82));
+Console.WriteLine(GetNumberSum(9012));
