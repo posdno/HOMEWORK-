b69da30f964b0a02fd749c26186bfe1eb735c2fe
posdno <molodkina.dasha@mail.ru>
2022-11-21 22:06:59 +0300

    Добавили код 13

---
+++ HW013/Program.cs
@@ -0,0 +1,27 @@
+﻿int [] GetNumbers (int count)
+{
+    int []array=new int [count];
+    int i=0;
+    while(i<count)
+    {
+        array[i]=new Random().Next(0, 100);
+        i++;
+    }
+    return array;
+}
+
+string ArrayTostring(int[]array)
+{
+    string result=String.Empty;
+    int i=0;
+    while(i<array.Length)
+    {
+        result=result+array[i]+" ";
+        i++;
+    }
+    return result;
+}
+
+Console.WriteLine(ArrayTostring(GetNumbers(5)));
+Console.WriteLine(ArrayTostring(GetNumbers(3)));
+
