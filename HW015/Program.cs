b5b8c2fca2cb4f52eac34d3a0eed15117ad552ff
posdno <molodkina.dasha@mail.ru>
2022-11-22 20:00:15 +0300

    Добавили код 15

---
+++ HW015/Program.cs
@@ -0,0 +1,44 @@
+﻿void FillArray (int[] array)
+{
+    for (int i = 0; i<array.Length; i++)
+    {
+        array[i] = new Random().Next(-100, 100);
+    }
+}
+
+int[] CreateAndFill(int count)
+{
+    int[] array = new int[count];
+    for (int i = 0; i<count; i++)
+    {
+        array[i] = new Random().Next(-100, 100);
+    }
+    return array;
+}
+
+void PrintArray (int[] numbers)
+{
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        Console.Write($"{numbers[i]} ");
+    }
+    Console.WriteLine();
+}
+int Sum (int[] array)
+{
+    int result = 0;
+    for(int i=0; i<array.Length; i++)
+    {
+        if (i%2==0)
+        result=result+array[i];
+    }
+    return result;
+}
+Console.Clear();
+int len = 10;
+int[] arr = CreateAndFill(len);
+FillArray(arr);
+PrintArray(arr);
+int result = Sum (arr);
+Console.WriteLine("Сумма элементов на нечетных позициях - ");
+Console.WriteLine(result);
