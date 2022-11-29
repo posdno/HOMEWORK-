6f795d6dfc515437229cc55a8c42f1ee23712764
posdno <molodkina.dasha@mail.ru>
2022-11-29 15:17:29 +0300

    Добавили код 28

---
+++ HW028/Program.cs
@@ -0,0 +1,16 @@
+﻿int m = Input("Введите M:");
+int n = Input("Введите N:");
+Console.WriteLine($"Cумма элементов от {m} до {n} = {SumNatNambers(m, n)}");
+
+int Input(string input)
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
+
+int SumNatNambers(int m, int n)
+{
+    if (m == n) return n;
+    return n+ SumNatNambers(m, n - 1);
+}
