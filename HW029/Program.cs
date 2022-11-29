38dd1fe3a45e9ecbcda625126754ed018d498338
posdno <molodkina.dasha@mail.ru>
2022-11-29 15:27:19 +0300

    Добавили 29

---
+++ HW029/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите два положительных числа: M и N");
+int m = Input("Введите M: ");
+int n = Input("Введите N: ");
+Console.WriteLine($"A({m}), {n}) = {Akkerman (m, n)}");
+
+int Input(string input)
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
+
+int Akkerman (int m, int n)
+{
+    if (m == 0) return n+1;
+    if (m > 0 && n == 0) return Akkerman (m - 1, 1);
+    else return Akkerman (m-1, Akkerman (m, n - 1));
+}
