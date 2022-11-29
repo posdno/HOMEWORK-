e77a1ba297795fe332b68486089d513658532d6c
posdno <molodkina.dasha@mail.ru>
2022-11-29 15:09:14 +0300

    Добавили код 27

---
+++ HW027/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите натуральное число N:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int m = 1;
+Console.WriteLine(NatNumbers(n, m));
+
+int NatNumbers(int n, int m)
+{
+    if (n == m) return n;
+    else 
+       Console.Write($"{NatNumbers(n, m + 1)}, ");
+    return m;
+}
